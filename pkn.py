3e511a21228d9b20a5e2d0844bc0f6add415f893
mmpro <marekmikolajczyk87@gmail.com>
2018-10-15 19:23:28 +0200

    player1 zmieniony na class

--- pkn.py
+++ pkn.py
@@ -8,32 +8,31 @@ zbior={1:'PAPIER',2:'KAMIEŃ',3:'NOŻYCE'}
 print('Wybierz 1 lub 2 albo 3')
 
 #zdefiniowanie wyboru player1
-wybor_player1=[]
-def player1_input():
-    player1=input()
-    if player1=='1':
-        z=zbior[1]
-        print('Wybrałeś:',z)
-        return wybor_player1.append(z)
-    elif player1=='2':
-        z=zbior[2]
-        print('Wybrałeś:', z)
-        return wybor_player1.append(z)
-    elif player1=='3':
-        z=zbior[3]
-        print('Wybrałeś:', z)
-        return wybor_player1.append(z)
-    else:
-        print('Nie ma takiej możliwości wybierz 1, 2 lub 3')
-        player1_input()
-player1_input()
+class class_player_one():
+    wybor_player1 = []
+    def __init__(self):
+        self.player1=input()
+
+    def player1_wariant(self):
+        if self.player1=='1':
+            z=zbior[1]
+            print('Wybrałeś:',z)
+            return self.wybor_player1.append(z)
+        elif self.player1=='2':
+            z=zbior[2]
+            print('Wybrałeś:', z)
+            return self.wybor_player1.append(z)
+        elif self.player1=='3':
+            z=zbior[3]
+            print('Wybrałeś:', z)
+            return self.wybor_player1.append(z)
+        else:
+            print('Nie ma takiej możliwości wybierz 1, 2 lub 3')
+            class_player_one().player1_wariant()
 
 
-
-
-
-
-
-
+p1=class_player_one()
+p1.player1_wariant()
+print(p1.wybor_player1)
 
 
